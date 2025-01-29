2468c21d43f2922848f724a72e8853fc1cfbdd1d
Tristan Eykens <Tristan.Eykens@student.pxl.be>
2025-01-29 10:47:34 +0100

    - Namen aangepast van de verdiepingen in het nieuwe navigatie component.

--- components/CameraNavigation.jsx
+++ components/CameraNavigation.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 
 const CameraNavigation = ({ onSwitchCamera }) => {
     const cameras = [
-        { id: 1, name: 'Tower1ViewCamera' },
-        { id: 2, name: 'Tower2ViewCamera' },
-        { id: 3, name: 'Tower3ViewCamera' },
-        { id: 4, name: 'Tower4ViewCamera' },
+        { id: 1, name: 'Tower1ViewCamera', floorName:'Home' },
+        { id: 2, name: 'Tower2ViewCamera', floorName:'Music' },
+        { id: 3, name: 'Tower3ViewCamera', floorName:'Books' },
+        { id: 4, name: 'Tower4ViewCamera', floorName:'Merchandise' },
     ];
 
     const [currentCameraIndex, setCurrentCameraIndex] = useState(0);
@@ -25,7 +25,7 @@ const CameraNavigation = ({ onSwitchCamera }) => {
     return (
         <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 flex items-center">
             <button className="border-none outline-none bg-beige-background cursor-pointer p-4 rounded-l-full text-lg hover:bg-black hover:text-beige-background transition-colors" onClick={() => switchCamera('prev')}>{"<"}</button>
-            <span className="border-none outline-none bg-beige-background cursor-pointer p-4 text-lg transition-colors">{cameras[currentCameraIndex].name}</span>
+            <span className="border-none outline-none bg-beige-background cursor-pointer p-4 text-lg transition-colors">{cameras[currentCameraIndex].floorName}</span>
             <button className="border-none outline-none bg-beige-background cursor-pointer p-4 rounded-r-full text-lg hover:bg-black hover:text-beige-background transition-colors" onClick={() => switchCamera('next')}>{">"}</button>
         </div>
     );
